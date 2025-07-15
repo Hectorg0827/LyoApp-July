3ad4a81f00a6fc144bd02bcb82a5b9a504bfff5c
Your Name <hector.garcia0827@gmail.com>
2025-07-15 16:51:27 -0400

    Create FeedModels.swift

---
+++ LyoApp/FeedModels.swift
@@ -0,0 +1,8 @@
+import SwiftUI
+import Foundation
+
+// MARK: - Feed Models
+// NOTE: This file is kept for future extensions
+// Main feed models are defined in Models.swift
+
+// Additional feed-specific models can be added here as needed
