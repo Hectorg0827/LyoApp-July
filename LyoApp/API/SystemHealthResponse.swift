8c5a6cf38b4f941482c7b63f7225dda520e65962
Hectorg0827 <hector.garcia0827@gmail.com>
2025-10-12 22:55:27 -0400

    Create SystemHealthResponse.swift

---
+++ LyoApp/API/SystemHealthResponse.swift
@@ -0,0 +1,4 @@
+// Duplicate placeholder (API/SystemHealthResponse.swift) retained as empty marker to avoid build reference; safe to delete after project purge.
+enum _Dup_API_SystemHealthResponse_Removed {}
+#if false
+#endif
